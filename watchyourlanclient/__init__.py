8930dacac60c9930d840371eb6a53b255d9eebcd
David Wahlstrom <david.wahlstrom@gmail.com>
2024-09-05 10:02:41 -0600

    bump version 2024.1008-alpha -> 2024.1009-alpha

--- watchyourlanclient/__init__.py
+++ watchyourlanclient/__init__.py
@@ -5,7 +5,7 @@ from typing import Optional
 from cachetools import cached, TTLCache
 
 # Version
-__version__ = "2024.1008-alpha"
+__version__ = "2024.1009-alpha"
 
 # Set up basic logging configuration
 logging.basicConfig(level=logging.INFO)
